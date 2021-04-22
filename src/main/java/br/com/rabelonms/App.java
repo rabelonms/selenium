0390d91dffff235ba5ef12d0c442e843fb0eddb9
Nícolas M. Rabelo Saraiva <81185191+rabelonms@users.noreply.github.com>
2021-04-22 11:36:21 -0300

    Initial commit

---
+++ src/main/java/br/com/rabelonms/App.java
@@ -0,0 +1,13 @@
+package br.com.rabelonms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
